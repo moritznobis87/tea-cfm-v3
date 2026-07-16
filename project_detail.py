560239d780be7fe51dbed69243a1290751b2d5af
moritznobis87 <148576931+moritznobis87@users.noreply.github.com>
2026-07-16 11:51:20 +0200

    löschen

--- project_detail.py
+++
@@ -1,4 +0,0 @@
-[theme]
-primaryColor = "#BE172B"
-font = "Inter:https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap, sans-serif"
-headingFont = "Inter:https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap, sans-serif"
